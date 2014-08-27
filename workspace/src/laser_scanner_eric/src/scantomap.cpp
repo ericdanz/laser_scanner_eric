a4cf7634277cba1b6de65c87c96ad89fe6621eac
Eric Danz <ericdanziger@gmail.com>
2014-08-27 09:32:27 -0400

    putting it into a class

--- workspace/src/laser_scanner_eric/src/scantomap.cpp
+++ workspace/src/laser_scanner_eric/src/scantomap.cpp
@@ -8,40 +8,58 @@
 nav_msgs::MapMetaData mmd;
 
 
+class oGridMaker {
+
+    ros::NodeHandle n;
+
+	void initialize(this)
+	{
+		ros::Publisher chatter_pub = n.advertise<nav_msgs::OccupancyGrid>("map_data", 1, true);
+
+
+  		ros::Subscriber sub = n.subscribe("scan_data", 10, this.chatterCallback);
+	}
+	//this is where the work gets done
+	nav_msgs::OccupancyGrid OGridFromLScan(const sensor_msgs::LaserScan::ConstPtr& msg, nav_msgs::MapMetaData mapmetad)
+	{	
+
+
+	}
+
+	
+	void chatterCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+	{
+  		//Figure out if I need to use another pointer for this msg
+
+  		nav_msgs::OccupancyGrid newGrid = OGridFromLScan(msg, mmd);
+
+ 	 	//remake publisher? or just constantly update a global map that then gets pushed each spin? 
+  		chatter_pub.publish(newGrid);
+
+	}
+
+
+} ogmaker;
 
 
 
-void chatterCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
-{
-  //Figure out if I need to use another pointer for this msg
 
-  nav_msgs::OccupancyGrid newGrid = OGridFromLScan(msg, mmd);
-
-  //remake publisher? or just constantly update a global map that then gets pushed each spin? 
-  chatter_pub.publish(newGrid);
-
-}
-
-//This is where the work gets done
-nav_msgs::OccupancyGrid OGridFromLScan(sensor_msgs::LaserScan lmsg, nav_msgs::MapMetaData mapmetad)
-{
-
-
-}
 
 
 int main(int argc, char **argv)
 {
   
   ros::init(argc, argv, "scantomap");
+  
+  ogmaker.initialize();
+  //ros::NodeHandle n;
 
-  ros::NodeHandle n;
+  //ros::Publisher chatter_pub = n.advertise<nav_msgs::OccupancyGrid>("map_data", 1, true);
 
- 
 
-  ros::Subscriber sub = n.subscribe("scan_data", 10, chatterCallback);
+  //ros::Subscriber sub = n.subscribe("scan_data", 10, chatterCallback);
 
-  ros::Publisher chatter_pub = n.advertise<nav_msgs::OccupancyGrid>("map_data", 1, true);
+  
 
   
  
