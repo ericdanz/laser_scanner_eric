923b96b9f0703eae628b6610cec18d0e3a35d2cd
Eric Danz <ericdanziger@gmail.com>
2014-08-27 15:12:07 -0400

    filling in class

--- workspace/src/laser_scanner_eric/src/scantomap.cpp
+++ workspace/src/laser_scanner_eric/src/scantomap.cpp
@@ -1,10 +1,14 @@
 #include "ros/ros.h"
+#include "std_msgs/Time.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/UInt32.h"
+#include "geometry_msgs/Quaternion.h"
+#include "geometry_msgs/Point.h"
+#include "geometry_msgs/Pose.h"
 #include "sensor_msgs/LaserScan.h"
 #include "nav_msgs/MapMetaData.h"
 #include "nav_msgs/OccupancyGrid.h"
 #include <sstream>
-
-
 
 
 
@@ -19,24 +23,62 @@ class oGridMaker {
 		oGridMaker(ros::NodeHandle &nh)
 		{
 			n = nh;
-			mapper_pub = n.advertise<nav_msgs::OccupancyGrid>("map_data", 1, true);
+            mapper_pub = n.advertise<nav_msgs::OccupancyGrid>("map_eric", 1, true);
 
 			// or this->scannerCallBack
-	  		sub = n.subscribe("scan_data", 10, scannerCallback);
-		}
-		//this is where the work gets done
-		nav_msgs::OccupancyGrid OGridFromLScan(const sensor_msgs::LaserScan::ConstPtr& msg, nav_msgs::MapMetaData mapmetad)
-		{	
+            sub = n.subscribe("scan", 10, &oGridMaker::scannerCallBack, this);
 
+            //make map meta data
+            mmd.map_load_time = ros::Time::now();
+            mmd.resolution = 1.0;
+            mmd.width = 10;
+            mmd.height = 5;
+            mmd.origin = &oGridMaker::createPose();
+
+		}
+
+        geometry_msgs::Pose createPose()
+        {
+            geometry_msgs::Point thisp;
+            thisp.x = -1.0;
+            thisp.y = -5.0;
+            thisp.z = 0.0;
+
+            geometry_msgs::Quaternion thisq;
+            thisq.x = 0.0;
+            thisq.y = 0.0;
+            thisq.z = 0.0;
+            thisq.w = 0.0;
+
+            geometry_msgs::Pose thispose;
+            thispose.position = thisp;
+            thispose.orientation = thisq;
+
+            return thispose;
+        }
+
+		//this is where the work gets done
+        nav_msgs::OccupancyGrid OGridFromLScan(const sensor_msgs::LaserScan::ConstPtr& msg, const nav_msgs::MapMetaData::ConstPtr& mapmetad)
+		{	
+            //update time
+            mapmetad->map_load_time = msg->header.stamp;
+
+
+            //angle to this scan, with distance, should determine which tiles it hit on as filled
+              //number of
+            for (i=0;i++;i < msg->ranges.size())
+            {
+
+            }
 
 		}
 
 	
-		void scannerCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+        void scannerCallBack(const sensor_msgs::LaserScan::ConstPtr& msg)
 		{
 	  		//Figure out if I need to use another pointer for this msg
 
-	  		mapGrid = OGridFromLScan(msg, mmd);
+            mapGrid = OGridFromLScan(msg, mmd);
 
 	 	 	//remake publisher? or just constantly update a global map that then gets pushed each spin? 
 	  		mapper_pub.publish(mapGrid);
@@ -44,7 +86,7 @@ class oGridMaker {
 		}
 
 
-} ogmaker;
+};
 
 
 
@@ -64,9 +106,6 @@ int main(int argc, char **argv)
 
   	//ros::Subscriber sub = n.subscribe("scan_data", 10, chatterCallback);
 
-  
-
-  
  
   	ros::spin();
 
